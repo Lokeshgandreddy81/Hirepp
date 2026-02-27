02750a804e875395d21c4eb5c54cef4b2eac32c1
lokeshgandreddy81 <lokeshgandreddy81@gmail.com>
2026-02-27 16:27:57 +0530

    core: enforce token-validated auto login and onboarding gating

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -174,15 +174,25 @@ const AppNav = () => {
           </>
         ) : (
           // Unauthenticated Stack
-          <>
-            {!hasCompletedOnboarding && <Stack.Screen name="Onboarding" component={OnboardingScreen} />}
-            <Stack.Screen name="RoleSelection" component={RoleSelectionScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-            <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
-            <Stack.Screen name="VerificationRequired" component={VerificationRequiredScreen} />
-          </>
+          hasCompletedOnboarding ? (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Register" component={RegisterScreen} />
+              <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+              <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+              <Stack.Screen name="VerificationRequired" component={VerificationRequiredScreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+              <Stack.Screen name="RoleSelection" component={RoleSelectionScreen} />
+              <Stack.Screen name="Register" component={RegisterScreen} />
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+              <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+              <Stack.Screen name="VerificationRequired" component={VerificationRequiredScreen} />
+            </>
+          )
         )}
       </Stack.Navigator>
       <StatusBar style="auto" />
